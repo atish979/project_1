e4827907a07dbead3e191ded42b60e8c355a7e59
atish979 <s65128892@gmail.com>
2026-08-24 13:22:01 +0530

    Add files via upload

---
+++ library.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+  int main() 
+   {
+      int id1,id2,id3;
+      string  t1,t2,t3;
+
+      
+      cout<<"enter id1";
+      cin>>id1;
+  
+      cin.ignore();
+      cout<<"Enter title"<<t1;
+      getline(cin, t1);
+    
+      cout<<"enter id2";
+      cin>>id2;
+      
+      cin.ignore();
+      cout<<"Enter title";
+      getline(cin,t2);
+  
+      cout<<"enter id3"<<id3;
+      cin>>id3;
+  
+      cin.ignore();
+      cout<<"Enter title"<<t3;
+      getline(cin,t3);
+
+     cout<< "===LIBRARY===";
+    
+ 
+
